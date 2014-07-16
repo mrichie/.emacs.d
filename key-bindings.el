75f23d32404409efe3ef69216d8041db7337fe37
Richie <minruiqi@gmail.com>
2014-07-16 21:56:59 +0800

    uncomment unused keys

--- key-bindings.el
+++ key-bindings.el
@@ -4,97 +4,97 @@
 (global-set-key (kbd "C-x C-c") 'delete-frame)
 
 ;; Completion that uses many different methods to find options.
-(global-set-key (kbd "C-.") 'hippie-expand-no-case-fold)
-(global-set-key (kbd "C-:") 'hippie-expand-lines)
-(global-set-key (kbd "C-,") 'completion-at-point)
+;; (global-set-key (kbd "C-.") 'hippie-expand-no-case-fold)
+;; (global-set-key (kbd "C-:") 'hippie-expand-lines)
+;; (global-set-key (kbd "C-,") 'completion-at-point)
 
 (require 'misc)
-(global-set-key (kbd "s-.") 'copy-from-above-command)
+;; (global-set-key (kbd "s-.") 'copy-from-above-command)
 
 ;; Smart M-x
-(global-set-key (kbd "M-x") 'smex)
-(global-set-key (kbd "M-X") 'smex-major-mode-commands)
-(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
+;; (global-set-key (kbd "M-x") 'smex)
+;; (global-set-key (kbd "M-X") 'smex-major-mode-commands)
+;; (global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
 
 ;; Use C-x C-m to do M-x per Steve Yegge's advice
-(global-set-key (kbd "C-x C-m") 'smex)
+;; (global-set-key (kbd "C-x C-m") 'smex)
 
 ;; Expand region (increases selected region by semantic units)
-(global-set-key (if is-mac (kbd "C-=")) 'er/expand-region)
+;; (global-set-key (if is-mac (kbd "C-=")) 'er/expand-region)
 
-;; Experimental multiple-cursors
-(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
-(global-set-key (kbd "C-S-c C-e") 'mc/edit-ends-of-lines)
-(global-set-key (kbd "C-S-c C-a") 'mc/edit-beginnings-of-lines)
+;; ;; Experimental multiple-cursors
+;; (global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+;; (global-set-key (kbd "C-S-c C-e") 'mc/edit-ends-of-lines)
+;; (global-set-key (kbd "C-S-c C-a") 'mc/edit-beginnings-of-lines)
 
-;; Mark additional regions matching current region
-(global-set-key (kbd "M-æ") 'mc/mark-all-dwim)
-(global-set-key (kbd "C-å") 'mc/mark-previous-like-this)
-(global-set-key (kbd "C-æ") 'mc/mark-next-like-this)
-(global-set-key (kbd "C-Æ") 'mc/mark-more-like-this-extended)
-(global-set-key (kbd "M-å") 'mc/mark-all-in-region)
+;; ;; Mark additional regions matching current region
+;; (global-set-key (kbd "M-æ") 'mc/mark-all-dwim)
+;; (global-set-key (kbd "C-å") 'mc/mark-previous-like-this)
+;; (global-set-key (kbd "C-æ") 'mc/mark-next-like-this)
+;; (global-set-key (kbd "C-Æ") 'mc/mark-more-like-this-extended)
+;; (global-set-key (kbd "M-å") 'mc/mark-all-in-region)
 
-;; Symbol and word specific mark-more
-(global-set-key (kbd "s-æ") 'mc/mark-next-word-like-this)
-(global-set-key (kbd "s-å") 'mc/mark-previous-word-like-this)
-(global-set-key (kbd "M-s-æ") 'mc/mark-all-words-like-this)
-(global-set-key (kbd "s-Æ") 'mc/mark-next-symbol-like-this)
-(global-set-key (kbd "s-Å") 'mc/mark-previous-symbol-like-this)
-(global-set-key (kbd "M-s-Æ") 'mc/mark-all-symbols-like-this)
+;; ;; Symbol and word specific mark-more
+;; (global-set-key (kbd "s-æ") 'mc/mark-next-word-like-this)
+;; (global-set-key (kbd "s-å") 'mc/mark-previous-word-like-this)
+;; (global-set-key (kbd "M-s-æ") 'mc/mark-all-words-like-this)
+;; (global-set-key (kbd "s-Æ") 'mc/mark-next-symbol-like-this)
+;; (global-set-key (kbd "s-Å") 'mc/mark-previous-symbol-like-this)
+;; (global-set-key (kbd "M-s-Æ") 'mc/mark-all-symbols-like-this)
 
-;; Extra multiple cursors stuff
-(global-set-key (kbd "C-~") 'mc/reverse-regions)
-(global-set-key (kbd "M-~") 'mc/sort-regions)
-(global-set-key (kbd "H-~") 'mc/insert-numbers)
+;; ;; Extra multiple cursors stuff
+;; (global-set-key (kbd "C-~") 'mc/reverse-regions)
+;; (global-set-key (kbd "M-~") 'mc/sort-regions)
+;; (global-set-key (kbd "H-~") 'mc/insert-numbers)
 
-(global-set-key (kbd "C-S-<mouse-1>") 'mc/add-cursor-on-click)
+;; (global-set-key (kbd "C-S-<mouse-1>") 'mc/add-cursor-on-click)
 
-;; Set anchor to start rectangular-region-mode
-(global-set-key (kbd "H-SPC") 'set-rectangular-region-anchor)
+;; ;; Set anchor to start rectangular-region-mode
+;; (global-set-key (kbd "H-SPC") 'set-rectangular-region-anchor)
 
-;; Replace rectangle-text with inline-string-rectangle
-(global-set-key (kbd "C-x r t") 'inline-string-rectangle)
+;; ;; Replace rectangle-text with inline-string-rectangle
+;; (global-set-key (kbd "C-x r t") 'inline-string-rectangle)
 
-;; Quickly jump in document with ace-jump-mode
-(define-key global-map (kbd "C-ø") 'ace-jump-mode)
+;; ;; Quickly jump in document with ace-jump-mode
+;; (define-key global-map (kbd "C-ø") 'ace-jump-mode)
 
-;; Perform general cleanup.
-(global-set-key (kbd "C-c n") 'cleanup-buffer)
-(global-set-key (kbd "C-c C-n") 'cleanup-buffer)
-(global-set-key (kbd "C-c C-<return>") 'delete-blank-lines)
+;; ;; Perform general cleanup.
+;; (global-set-key (kbd "C-c n") 'cleanup-buffer)
+;; (global-set-key (kbd "C-c C-n") 'cleanup-buffer)
+;; (global-set-key (kbd "C-c C-<return>") 'delete-blank-lines)
 
-;; M-i for back-to-indentation
-(global-set-key (kbd "M-i") 'back-to-indentation)
+;; ;; M-i for back-to-indentation
+;; (global-set-key (kbd "M-i") 'back-to-indentation)
 
-;; Turn on the menu bar for exploring new modes
-(global-set-key (kbd "C-<f10>") 'menu-bar-mode)
+;; ;; Turn on the menu bar for exploring new modes
+;; (global-set-key (kbd "C-<f10>") 'menu-bar-mode)
 
-;; Use shell-like backspace C-h, rebind help to F1
-(define-key key-translation-map [?\C-h] [?\C-?])
-(global-set-key (kbd "<f1>") 'help-command)
+;; ;; Use shell-like backspace C-h, rebind help to F1
+;; (define-key key-translation-map [?\C-h] [?\C-?])
+;; (global-set-key (kbd "<f1>") 'help-command)
 
-(global-set-key (kbd "M-h") 'kill-region-or-backward-word)
+;; (global-set-key (kbd "M-h") 'kill-region-or-backward-word)
 
-;; Transpose stuff with M-t
-(global-unset-key (kbd "M-t")) ;; which used to be transpose-words
-(global-set-key (kbd "M-t l") 'transpose-lines)
-(global-set-key (kbd "M-t w") 'transpose-words)
-(global-set-key (kbd "M-t s") 'transpose-sexps)
-(global-set-key (kbd "M-t p") 'transpose-params)
+;; ;; Transpose stuff with M-t
+;; (global-unset-key (kbd "M-t")) ;; which used to be transpose-words
+;; (global-set-key (kbd "M-t l") 'transpose-lines)
+;; (global-set-key (kbd "M-t w") 'transpose-words)
+;; (global-set-key (kbd "M-t s") 'transpose-sexps)
+;; (global-set-key (kbd "M-t p") 'transpose-params)
 
-;; Interactive selective display
-(global-set-key (kbd "C-x $") 'inc-selective-display)
+;; ;; Interactive selective display
+;; (global-set-key (kbd "C-x $") 'inc-selective-display)
 
-;; Change next underscore with a camel case
-(global-set-key (kbd "C-c C--") 'replace-next-underscore-with-camel)
-(global-set-key (kbd "M-s M--") 'snakeify-current-word)
+;; ;; Change next underscore with a camel case
+;; (global-set-key (kbd "C-c C--") 'replace-next-underscore-with-camel)
+;; (global-set-key (kbd "M-s M--") 'snakeify-current-word)
 
-;; Change word separators
-(global-unset-key (kbd "C-x +")) ;; used to be balance-windows
-(global-set-key (kbd "C-x + -") (λ (replace-region-by 's-dashed-words)))
-(global-set-key (kbd "C-x + _") (λ (replace-region-by 's-snake-case)))
-(global-set-key (kbd "C-x + c") (λ (replace-region-by 's-lower-camel-case)))
-(global-set-key (kbd "C-x + C") (λ (replace-region-by 's-upper-camel-case)))
+;; ;; Change word separators
+;; (global-unset-key (kbd "C-x +")) ;; used to be balance-windows
+;; (global-set-key (kbd "C-x + -") (λ (replace-region-by 's-dashed-words)))
+;; (global-set-key (kbd "C-x + _") (λ (replace-region-by 's-snake-case)))
+;; (global-set-key (kbd "C-x + c") (λ (replace-region-by 's-lower-camel-case)))
+;; (global-set-key (kbd "C-x + C") (λ (replace-region-by 's-upper-camel-case)))
 
 ;; Killing text
 (global-set-key (kbd "C-S-k") 'kill-and-retry-line)
@@ -247,11 +247,11 @@
 (global-set-key (kbd "<s-down>") 'windmove-down)
 
 ;; Magit
-(global-set-key (kbd "C-x m") 'magit-status)
-(autoload 'magit-status "magit")
+;; (global-set-key (kbd "C-x m") 'magit-status)
+;; (autoload 'magit-status "magit")
 
 ;; Mu4e
-(global-set-key (kbd "C-x M") 'mu4e-up-to-date-status)
+;; (global-set-key (kbd "C-x M") 'mu4e-up-to-date-status)
 
 ;; Clever newlines
 (global-set-key (kbd "C-o") 'open-line-and-indent)
@@ -340,8 +340,8 @@
 ;;(global-set-key (kbd "C-x C-o !") (ffip-create-pattern-file-finder "*"))
 
 ;; View occurrence in occur mode
-(define-key occur-mode-map (kbd "v") 'occur-mode-display-occurrence)
-(define-key occur-mode-map (kbd "n") 'next-line)
-(define-key occur-mode-map (kbd "p") 'previous-line)
+;; (define-key occur-mode-map (kbd "v") 'occur-mode-display-occurrence)
+;; (define-key occur-mode-map (kbd "n") 'next-line)
+;; (define-key occur-mode-map (kbd "p") 'previous-line)
 
 (provide 'key-bindings)
